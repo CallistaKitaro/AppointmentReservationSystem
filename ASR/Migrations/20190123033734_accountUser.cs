8477c0e79f3230631267724b1a65faa405749f78
Callista Kitaro <callista.kitaro@gmail.com>
2019-01-23 15:08:15 +1100

    Revert "to revert"
    This reverts commit 0296cc8e4033b1788bfbc5497a16e200546079c6.

---
+++ ASR/Migrations/20190123033734_accountUser.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASR.Migrations
+{
+    public partial class accountUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Staff",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<string>(
+                name: "StaffID",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentID",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Admin",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_StaffID",
+                table: "AspNetUsers",
+                column: "StaffID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_StudentID",
+                table: "AspNetUsers",
+                column: "StudentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Staff_StaffID",
+                table: "AspNetUsers",
+                column: "StaffID",
+                principalTable: "Staff",
+                principalColumn: "StaffID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Student_StudentID",
+                table: "AspNetUsers",
+                column: "StudentID",
+                principalTable: "Student",
+                principalColumn: "StudentID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Staff_StaffID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Student_StudentID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_StaffID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_StudentID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StaffID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StudentID",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Staff",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Admin",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
